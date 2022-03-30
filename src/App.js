5fe69a27db7f80bcfc380678e9116c7a74df18d4
Alisha Soriano <sorianoalisha@gmail.com>
2022-03-30 12:58:22 -0400

    Create starting page component

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Start from './components/Start';
+
+export default function App() {
+  return (
+    <Start />
+  )
+}
---
+++ src/components/Start.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Start() {
+  return (
+    <div className='wrapper'>
+      <h2>Quizzical</h2>
+      <p>Can you answer all 5 questions correctly? We'll see!</p>
+      <button className='start-btn'>Start Quiz</button>
+    </div>
+  )
+}
