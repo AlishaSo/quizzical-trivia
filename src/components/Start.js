2669c258001525cbc1e6e393a30013e76a7ef7ee
Alisha Soriano <sorianoalisha@gmail.com>
2022-03-30 15:18:46 -0400

    Add a class to the title and another to the game description

--- src/components/Start.js
+++ src/components/Start.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Start() {
   return (
     <div className='wrapper'>
-      <h2>Quizzical</h2>
-      <p>Can you answer all 5 questions correctly? We'll see!</p>
+      <h2 className='game-title'>Quizzical</h2>
+      <p className='game-description'>Can you answer all 5 questions correctly? We'll see!</p>
       <button className='start-btn'>Start Quiz</button>
     </div>
   )
