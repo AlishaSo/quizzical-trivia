265b1aaf29349584faff6a591aee68f3a0f7a261
Alisha Soriano <sorianoalisha@gmail.com>
2022-03-31 13:45:25 -0400

    Add an effect that handles the fetch call

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ import Quiz from './components/Quiz';
 export default function App() {
   const [gameOn, setGameOn] = useState(false);
 
+  useEffect(() => {
+    try {
+      fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=medium')
+        .then(response => response.json())
+        .then(data => {
+          if(data.response_code !== 0) {
+            throw new Error('Could not retrieve any questions at this time');
+          } else {
+            console.log(data.results);
+          }
+        });
+    } catch(error) {
+      window.alert(error);
+    }
+  }, [gameOn]);
+
   function startClick() {
     setGameOn(true);
   }
