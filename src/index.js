82e657922813a85e09251ce8a5060e2668803fbb
Alisha Soriano <sorianoalisha@gmail.com>
2022-03-31 14:01:43 -0400

    Edit code to render app according to React 18 documentation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client'; //needed for new version of React: 18
 import App from './App';
 import './styles.css';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+//in React 18, the below replaces the old way of rendering your app
+const container = document.querySelector('#root');
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<App />);
