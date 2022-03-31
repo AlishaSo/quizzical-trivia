3f9afce521168a8450cb370c6d2dcd21e9eddd1c
Alisha Soriano <sorianoalisha@gmail.com>
2022-03-31 10:48:23 -0400

    Add state variable to conditionally render either the Quiz or the Start component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Start from './components/Start';
+import Quiz from './components/Quiz';
 
 export default function App() {
+  const [gameOn, setGameOn] = useState(false);
+
   return (
-    <Start />
+    <>
+      { gameOn ? <Quiz /> : <Start /> }
+    </>
   )
 }
