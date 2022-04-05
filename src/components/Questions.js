5a6271ce0fbaad47de30f1bada0782aaae49b160
Alisha Soriano <sorianoalisha@gmail.com>
2022-04-05 15:55:30 -0400

    Add the buttons to choose from the answer options

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 export default function Questions(props) {
+  let allAnswers = props.allAnswers;
+  allAnswers = allAnswers.sort(() => Math.random() - 0.5);
+  const answerOptions = allAnswers.map(answer => {
+    const correctAnswerClass = answer === props.correctAnswer ? ' correct-answer' : '';
+    return <button className={`option-btn${correctAnswerClass}`}>{answer}</button>
+  })
+
   return (
     <div className='quest-el-div'>
       <h2>{props.question}</h2>
+      <div className='answers-container'>{answerOptions}</div>
       <h3>{props.correctAnswer}</h3>
     </div>
   )
