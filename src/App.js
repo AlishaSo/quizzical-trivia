4d0a27d5d4fbe49d767b30931b3b50508d098654
Alisha Soriano <sorianoalisha@gmail.com>
2022-07-31 16:28:21 -0400

    Change the return container to be a div instead of a fragment and add a class name

--- src/App.js
+++ src/App.js
@@ -5,37 +5,13 @@ import Quiz from './components/Quiz';
 export default function App() {
   const [gameOn, setGameOn] = useState(false);
 
-  // useEffect(() => {
-  //   try {
-  //     fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple')
-  //       .then(response => response.json())
-  //       .then(data => {
-  //         if(data.response_code !== 0) {
-  //           throw new Error('Could not retrieve any questions at this time');
-  //         } else {
-  //           // console.log(data.results);
-  //           const triviaArr = data.results.map(item => ({
-  //             question: item.question,
-  //             answers: item.incorrect_answers.concat(item.correct_answer),
-  //             rightAnswer: item.correct_answer
-  //         }))
-  //           setTriviaData(triviaArr);
-  //         }
-  //       });
-  //   } catch(error) {
-  //     window.alert(error);
-  //   }
-  // }, []);
-
   function toggleGame() {
     setGameOn(true);
   }
 
   return (
-    <>
+    <div className='App'>
       { gameOn ? <Quiz /> : <Start toggleGame={toggleGame} /> }
-    </>
+    </div>
   )
 }
-
-//<Quiz trivia={triviaData} /> : <Start toggleGame={toggleGame}
