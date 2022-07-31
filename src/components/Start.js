ac2f4ff14c336e4460bedca52bb1fe8270385f21
Alisha Soriano <sorianoalisha@gmail.com>
2022-07-31 16:32:37 -0400

    Add a class of button to the button for starting the quiz

--- src/components/Start.js
+++ src/components/Start.js
@@ -5,7 +5,7 @@ export default function Start(props) {
     <div className='wrapper'>
       <h2 className='game-title'>Quizzical</h2>
       <p className='game-description'>Can you answer all 5 questions correctly? We'll see!</p>
-      <button onClick={props.toggleGame} className='start-btn'>Start Quiz</button>
+      <button onClick={props.toggleGame} className='start-btn btn'>Start Quiz</button>
     </div>
   )
 }
