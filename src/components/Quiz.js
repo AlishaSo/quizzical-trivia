d4c6d99dcc459b3fd9f6a158f0d3c8d2e25d2618
Alisha Soriano <sorianoalisha@gmail.com>
2022-10-03 13:05:05 -0400

    Create a map that holds all of the correct answers, and pass it to the checkAnswers function upon quiz submission

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,14 @@
-import { useState, useEffect } from 'react';
-import getData from '../services/getQuizData';
-import Questions from './Questions';
-import { nanoid } from 'nanoid';
+import { useState, useEffect } from "react";
+import getData from "../services/getQuizData";
+import Questions from "./Questions";
+import { nanoid } from "nanoid";
+import checkAnswers from "../services/checkAnswers";
+import { decode } from "html-entities";
 
 export default function Quiz() {
   const [triviaData, setTriviaData] = useState([]);
+  const [submitStatus, setSubmitStatus] = useState(false);
+  const [correctAnswers, setCorrectAnswers] = useState(new Map());
 
   useEffect(() => {
     async function callApi() {
@@ -14,19 +18,40 @@ export default function Quiz() {
     callApi();
   }, []);
 
-  const questionElements = triviaData.map(item => (
-    <Questions 
-      key = {nanoid()}
-      question = {item.question}
-      allAnswers = {item.answers}
-      correctAnswer = {item.rightAnswer}
-    />)
-  )
-  
+  useEffect(() => {
+    createCorrectAnswersMap();
+  }, [triviaData]);
+
+  const questionElements = triviaData.map(item => {
+    const nan = nanoid();
+    return (
+      <Questions
+        key={nan}
+        id={nan}
+        question={item.question}
+        allAnswers={item.answers}
+        correctAnswer={item.rightAnswer}
+      />
+    );
+  });
+
+  const createCorrectAnswersMap = () => {
+    triviaData.forEach((item, index) => {
+      setCorrectAnswers(
+        new Map(correctAnswers.set(decode(item.rightAnswer), `q${index}`))
+      );
+    });
+  };
+
   return (
-    <div className='quiz-container'>
+    <div className="quiz-container">
       {questionElements}
-      <button className='btn check-answers-btn'>Check answers</button>
+      <button
+        className="btn check-answers-btn"
+        onClick={() => checkAnswers(correctAnswers)}
+      >
+        Check answers
+      </button>
     </div>
-  )
+  );
 }
