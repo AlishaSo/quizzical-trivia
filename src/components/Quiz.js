201354887956cfefdc2abea68a8083b841cbfda3
Alisha Soriano <sorianoalisha@gmail.com>
2022-10-11 12:00:20 -0400

    Add a state variable to keep track of the user's selected answers; create a function for both when a user selects an answer and another for when the quiz is submitted; pass both functions to the Questions component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -9,6 +9,13 @@ export default function Quiz() {
   const [triviaData, setTriviaData] = useState([]);
   const [submitStatus, setSubmitStatus] = useState(false);
   const [correctAnswers, setCorrectAnswers] = useState(new Map());
+  const [userChoices, setUserChoices] = useState({
+    q0: '',
+    q1: '',
+    q2: '',
+    q3: '',
+    q4: ''
+  });
 
   useEffect(() => {
     async function callApi() {
@@ -22,6 +29,15 @@ export default function Quiz() {
     createCorrectAnswersMap();
   }, [triviaData]);
 
+  const selectAnswer = (questionIndex, answer) => {
+    console.log('selectAnswer called');
+    setUserChoices(prevChoices => {
+      let newAnswer = {};
+      newAnswer[`q${questionIndex}`] = answer;
+      return Object.assign(prevChoices, newAnswer);
+    })
+  }
+
   const questionElements = triviaData.map(item => {
     const nan = nanoid();
     return (
@@ -31,6 +47,8 @@ export default function Quiz() {
         question={item.question}
         allAnswers={item.answers}
         correctAnswer={item.rightAnswer}
+        submitStatus={submitStatus}
+        handleClick={selectAnswer}
       />
     );
   });
@@ -43,12 +61,19 @@ export default function Quiz() {
     });
   };
 
+  const submitQuiz = () => {
+    setSubmitStatus(prevStatus => !prevStatus);
+    checkAnswers(correctAnswers);
+  }
+
+  console.log(userChoices)
+
   return (
     <div className="quiz-container">
       {questionElements}
       <button
         className="btn check-answers-btn"
-        onClick={() => checkAnswers(correctAnswers)}
+        onClick={submitQuiz}
       >
         Check answers
       </button>
